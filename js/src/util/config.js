2c79c96f59e117ff1fccc7ceb3fbb7fc57396a9d
XhmikosR <xhmikosr@gmail.com>
2021-09-28 19:46:42 +0300

    Update config.js

--- js/src/util/config.js
+++ js/src/util/config.js
@@ -1,7 +1,7 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v5.0.2): util/config.js
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * Bootstrap (v5.1.1): util/config.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
 
